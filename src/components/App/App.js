5bff0d48ffe8a337ea667ffff8dbdcf949ace057
Seth Burns <seth@sethburns.com>
2023-08-09 10:51:35 -0700

    Add ability to hide instructions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,42 @@
+import { useState } from 'react';
 import { Form } from '../Form/Form';
 import './App.css';
 
 function App() {
+  const [hideButtonText, setHideButtonText] = useState('Hide Instructions');
+  const [instructionsHidden, setInstructionsHidden] = useState(false);
+
+  function handleHideClick(e) {
+    e.preventDefault();
+    if (!instructionsHidden) {
+      setHideButtonText('Show Instuctions');
+      setInstructionsHidden(true)
+    } else {
+      setHideButtonText('Hide Instructions');
+      setInstructionsHidden(false)
+    }
+  }
+
   return (
     <div className="App">
       <h1 className="wordle-solver-title">WORDLE SOLVER</h1>
-      <h2>
-        Enter green and yellow letters in their corresponding places.<br></br> For
-        example, a green "K" on letter 3 should be placed in the middle green
-        square. <br></br> A yellow "D" on letter 2 should be placed in the yellow box second from the left. <br></br>
+      {!instructionsHidden && <h2>
+        Enter green and yellow letters in their corresponding places.<br></br>{' '}
+        For example, a green "K" on letter 3 should be placed in the middle
+        green square. <br></br> A yellow "D" on letter 2 should be placed in the
+        yellow box second from the left. <br></br>
         Enter all black letters in any order. <br></br>
-        No commas between letters. <br></br> One letter per green input, as many in other inputs as needed.
-      </h2>
+        No commas between letters. <br></br> One letter per green input, as many
+        in other inputs as needed.
+      </h2>}
+      <button
+        id="hide"
+        onClick={(e) => {
+          handleHideClick(e);
+        }}
+      >
+        {hideButtonText}
+      </button>
       <Form />
     </div>
   );
